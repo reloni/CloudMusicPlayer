da6ef176df2a95e88b9940c32de6f8af5f3dcfcd
Anton Efimenko <reloni@ya.ru>
2016-01-23 20:59:54 +0300

    Добавлена простая авторизация для яндекса.

--- CloudMusicPlayer/AppDelegate.swift
+++ CloudMusicPlayer/AppDelegate.swift
@@ -19,6 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Override point for customization after application launch.
 		return true
 	}
+	
+	// вызывается при вызове приложения по URL схеме
+	// настраивается в  Info.plist в разделе URL types/URL Schemes
+	func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+		//print(url)
+		if let navigationController = window?.rootViewController as? UINavigationController {
+			navigationController.popViewControllerAnimated(true)
+		}
+		return true
+	}
 
 	func applicationWillResignActive(application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- CloudMusicPlayer/LoginController.swift
+++ CloudMusicPlayer/LoginController.swift
@@ -1,32 +1,35 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  CloudMusicPlayer
 //
-//  Created by Anton Efimenko on 19.01.16.
+//  Created by Anton Efimenko on 23.01.16.
 //  Copyright © 2016 Anton Efimenko. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
+	@IBOutlet weak var webView: UIWebView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		//print("didload")
 		// Do any additional setup after loading the view, typically from a nib.
+		let url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=6556b9ed6fb146ea824d2e1f0d98f09b"
+		if let nsUrl = NSURL(string: url) {
+			webView.loadRequest(NSURLRequest(URL: nsUrl))
+		}
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	
 //	@IBAction func logIn(sender: AnyObject) {
 //		let url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=6556b9ed6fb146ea824d2e1f0d98f09b"
 //		//let url = "https://www.google.com";
 //		//UIApplication.sharedApplication().openURL(NSURL(fileURLWithPath: url))
 //		webView.loadRequest(NSURLRequest(URL: NSURL(string: url)!))
 //	}
-
+	
 }
-
