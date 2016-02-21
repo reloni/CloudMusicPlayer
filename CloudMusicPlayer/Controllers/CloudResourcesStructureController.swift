c214e0419f98e949933b5332e8d25eadc3bdf41a
Anton Efimenko <reloni@ya.ru>
2016-02-21 17:31:47 +0300

    Added controller, model and table view

---
+++ CloudMusicPlayer/Controllers/CloudResourcesStructureController.swift
@@ -0,0 +1,21 @@
+//
+//  CloudResourcesStructureController.swift
+//  CloudMusicPlayer
+//
+//  Created by Anton Efimenko on 21.02.16.
+//  Copyright © 2016 Anton Efimenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CloudResourcesStructureController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+}
---
+++ CloudMusicPlayer/Models/CloudResourcesModel.swift
@@ -0,0 +1,9 @@
+//
+//  CloudResourcesModel.swift
+//  CloudMusicPlayer
+//
+//  Created by Anton Efimenko on 21.02.16.
+//  Copyright © 2016 Anton Efimenko. All rights reserved.
+//
+
+import Foundation
