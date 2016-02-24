022be06e0a71c7c43756c49e10a3905924f6c28f
Anton Efimenko <reloni@ya.ru>
2016-02-24 23:05:11 +0300

    Add view for second folder tree level (not working now)

--- CloudMusicPlayer/Controllers/CloudResourcesStructureController.swift
+++ CloudMusicPlayer/Controllers/CloudResourcesStructureController.swift
@@ -25,6 +25,7 @@ class CloudResourcesStructureController: UIViewController {
     //let url = "https://cloud-api.yandex.net:443/v1/disk"
     //		let url = "https://cloud-api.yandex.net:443/v1/disk/resources?path=%2F"
     let url = "https://cloud-api.yandex.net:443/v1/disk/resources?path=/"
+		//let url = "https://cloud-api.yandex.net:443/v1/disk/resources?path=disk:/Documents"
     guard let token = OAuthResourceBase.Yandex.tokenId else {
       resourceContent = nil
       self.tableView.reloadData()
@@ -51,6 +52,17 @@ class CloudResourcesStructureController: UIViewController {
 }
 
 extension CloudResourcesStructureController : UITableViewDelegate {
+	
+	func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+//		UIViewController *myController = [self.storyboard instantiateViewControllerWithIdentifier:@"MyGenericTableViewController"];
+//		[self.navigationController pushViewController: myController animated:YES];
+		guard let controller = storyboard?.instantiateViewControllerWithIdentifier("DetailsView") as? DetailsController else {
+			return
+		}
+		controller.directory = resourceContent?["_embedded"]["items"][indexPath.row]["path"].string
+		navigationController?.pushViewController(controller, animated: true)
+	}
+	
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return resourceContent?["_embedded"]["items"].count ?? 0
   }
