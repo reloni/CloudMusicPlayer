c579654461ed44533bf0b603b348714168621486
Anton Efimenko <reloni@ya.ru>
2016-03-19 21:17:25 +0300

    Add test checking correct data received

--- CloudMusicPlayerTests/StreamDataTaskTests.swift
+++ CloudMusicPlayerTests/StreamDataTaskTests.swift
@@ -42,6 +42,43 @@ class StreamDataTaskTests: XCTestCase {
 		streamObserver = nil
 	}
 	
+	func testReceiveCorrectData() {
+		// set request url to check
+		request.URL = NSURL(string: "https://test.com")
+		
+		let testData = ["First", "Second", "Third", "Fourth"]
+		var dataSended:UInt64 = 0
+		
+		session.task?.taskProgress.bindNext { [unowned self] progress in
+			if case .resume(let tsk) = progress {
+				XCTAssertEqual(tsk.originalRequest?.URL, self.request.URL, "Check correct task url")
+				dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)) { [unowned self] in
+					for i in 0...testData.count - 1 {
+						let sendData = testData[i].dataUsingEncoding(NSUTF8StringEncoding)!
+						dataSended += UInt64(sendData.length)
+						self.streamObserver.sessionEvents.onNext(.didReceiveData(session: self.session, dataTask: tsk, data: sendData))
+					}
+					self.streamObserver.sessionEvents.onNext(.didCompleteWithError(session: self.session, dataTask: tsk, error: nil))
+				}
+			}
+			}.addDisposableTo(bag)
+		
+		var receiveCounter = 0
+		let expectation = expectationWithDescription("Should return correct data")
+		httpClient.loadStreamData(request, sessionConfiguration: .defaultSessionConfiguration()).bindNext { result in
+			if case .StreamedData(let data) = result {
+				XCTAssertEqual(String(data: data, encoding: NSUTF8StringEncoding), testData[receiveCounter], "Check correct chunk of data received")
+				receiveCounter += 1
+			} else if case .Success(let dataReceived) = result {
+				XCTAssertEqual(dataReceived, dataSended, "Should receive correct amount of data")
+				XCTAssertEqual(receiveCounter, testData.count, "Should receive correct amount of data chuncks")
+				expectation.fulfill()
+			}
+			}.addDisposableTo(bag)
+		
+		waitForExpectationsWithTimeout(1, handler: nil)
+	}
+	
 	func testReturnNSError() {
 		// set request url to check
 		request.URL = NSURL(string: "https://test.com")
