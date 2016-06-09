e426fa655112614051be2b523df28f79868d131a
Anton Efimenko <reloni@ya.ru>
2016-06-09 12:20:18 +0300

    Add observable extension for retryWithDelay and unit tests
    Add retryWithDelay(delayTime: Double, maxAttemptCount: Int,
    retryReturnObject: Observable.E, errorEvaluator: (ErrorType) -> Bool)
    -> Observable<Observable.E> extension method and unit tests.
    
    Also add unit tests for Yandex downloadUrl observable property.

--- CloudMusicPlayer/CloudResource/YandexDiskCloudAudioJsonResource.swift
+++ CloudMusicPlayer/CloudResource/YandexDiskCloudAudioJsonResource.swift
@@ -11,6 +11,9 @@ import SwiftyJSON
 import RxSwift
 
 public class YandexDiskCloudAudioJsonResource : YandexDiskCloudJsonResource, CloudAudioResource {
+	internal var downloadUrlErrorRetryDelayTime: Double = 1
+	internal var downloadUrlRetryMaxAttemptCount = 5
+	
 	internal var downloadResourceUrl: NSURL? {
 		return NSURL(baseUrl: resourcesUrl + "/download", parameters: getRequestParameters())
 	}
@@ -23,27 +26,7 @@ public class YandexDiskCloudAudioJsonResource : YandexDiskCloudJsonResource, Clo
 		let request = httpClient.httpUtilities.createUrlRequest(url, headers: getRequestHeaders())
 		return Observable.create { [weak self] observer in
 			guard let object = self else { observer.onCompleted(); return NopDisposable.instance }
-			/*
-			let task = self.httpClient.loadJsonData(request)
-				.doOnCompleted { _ in observer.onCompleted() }.bindNext { result in
-					if case Result.success(let box) = result {
-						if let href = box.value["href"].string {
-							observer.onNext(href)
-						}
-					} else if case Result.error(let error) = result {
-						print("yandex file url error: \(error)")
-						observer.onCompleted()
-					}
-			}
-			*/
 			
-			let concurrentScheduler = ConcurrentDispatchQueueScheduler(globalConcurrentQueueQOS: DispatchQueueSchedulerQOS.Utility)
-			let serialScheduler = SerialDispatchQueueScheduler(globalConcurrentQueueQOS: DispatchQueueSchedulerQOS.Utility)
-			var retryCounter = 0
-			
-			// perform request for downloadurl
-			// if Yandex server returned error "tooManyRequests"
-			// sleep and try again
 			let task = object.httpClient.loadJsonData(request).flatMapLatest { result -> Observable<String?> in
 				if case Result.success(let box) = result {
 					// check server side error
@@ -57,18 +40,12 @@ public class YandexDiskCloudAudioJsonResource : YandexDiskCloudJsonResource, Clo
 				}
 				// if no error returned and no href key in JSON, return nil
 				return Observable.just(nil)
-				} // retries if error occurred
-				.retryWhen { (error: Observable<ErrorType>) -> Observable<JSON> in
-					return error.observeOn(serialScheduler).flatMapLatest { returnedError -> Observable<JSON> in
-						// if server returned error "tooManyRequests", perform retry after delay
-						if case YandexDiskError.tooManyRequests = returnedError where retryCounter < 5 {
-							retryCounter += 1
-							return Observable.just(JSON([])).delaySubscription(1, scheduler: concurrentScheduler)
-						}
-						return Observable.error(returnedError)
-					}
-				}.catchError { _ in observer.onCompleted(); return Observable.empty() }
-				.bindNext { link in
+				}.retryWithDelay(object.downloadUrlErrorRetryDelayTime, maxAttemptCount: object.downloadUrlRetryMaxAttemptCount, retryReturnObject: "") { error in
+				if case YandexDiskError.tooManyRequests = error {
+					return true
+				}
+				return false
+				}.doOnError { _ in observer.onCompleted() }.bindNext { link in
 					guard let link = link else { observer.onCompleted(); return }
 					observer.onNext(link)
 					observer.onCompleted()
