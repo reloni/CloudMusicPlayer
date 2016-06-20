6e46095ef69ef9bf8649bebacbc2d5f837ed59d5
Anton Efimenko <reloni@ya.ru>
2016-06-20 11:42:35 +0300

    Delete some prints and commented code

--- CloudMusicPlayer/AppDelegate.swift
+++ CloudMusicPlayer/AppDelegate.swift
@@ -70,8 +70,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		MainModel.sharedInstance.loadPlayerState()
 		
-		//MainModel.sharedInstance.player.setUIApplication(UIApplication.sharedApplication())
-		
 		MainModel.sharedInstance.player.playerEvents.bindNext { event in
 			switch event {
 			case PlayerEvents.Stopped: fallthrough
@@ -144,26 +142,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillResignActive(application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-		print("applicationWillResignActive")
-		
-		//MainModel.sharedInstance.savePlayerState()
 	}
 
 	func applicationDidEnterBackground(application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-		//SharedSettings.Instance.saveData()
-		print("applicationDidEnterBackground")
 	}
 
 	func applicationWillEnterForeground(application: UIApplication) {
 		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-		print("applicationWillEnterForeground")
 	}
 
 	func applicationDidBecomeActive(application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-		print("applicationDidBecomeActive")
 	}
 
 	func applicationWillTerminate(application: UIApplication) {
